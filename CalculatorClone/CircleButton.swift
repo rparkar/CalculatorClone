b28324d5acd88dadc5c57f985ff2255852e0d136
Rehan Parkar <parkarcamp@gmail.com>
2018-02-28 20:57:28 -0600

    created circular button

---
+++ CalculatorClone/CircleButton.swift
@@ -0,0 +1,22 @@
+//
+//  CircleButton.swift
+//  CalculatorClone
+//
+//  Created by Rehan Parkar on 2018-02-28.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.cornerRadius = frame.height / 2
+        
+    }
+
+
+}
