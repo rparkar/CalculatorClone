bab0ce02b130385c8c062dbb186dd9553ecf60db
Rehan Parkar <parkarcamp@gmail.com>
2018-02-28 22:19:16 -0600

    buttons connected to the controller

--- CalculatorClone/Controller/ViewController.swift
+++ CalculatorClone/Controller/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //outlets
+    
+    @IBOutlet weak var displayAreaLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func numberButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pointButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func divideButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func multiplyButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func subtractButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func equalsButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func allClearButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func changeSignButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func percentageButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
 }
 
